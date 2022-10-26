6db623721867b52ac93dad4c7235b36e44b112f9
eniku <enikuauth@gmail.com>
2022-10-26 21:58:53 +0100

    chore(config.sh): clear script to accomodate new configuration

--- config.sh
+++ config.sh
@@ -2,15 +2,16 @@
 
 # Debian-Based Distros
 
-GLOBAL_PACKAGES=("nodejs" "npm" "neovim" "code" "kitty" "default-jdk" "python3-pip" "neofetch")
-REQUIRED_NODE_PACKAGES=("yarn")
+#GLOBAL_PACKAGES=("nodejs" "npm" "neovim" "code" "kitty" "default-jdk" "python3-pip" "neofetch")
+#REQUIRED_NODE_PACKAGES=("yarn")
 
 DEBIAN_DOTFILES=(".bashrc")
 DEBIAN_CONFIG_DOTFILES=("nvim" "kitty" "neofetch")
 
-ARCH_DOTFILES=(".xinitrc" ".screenlayout" ".zshrc" ".zprofile" ".bash_profile" ".gitconfig")
-ARCH_CONFIG_DOTFILES=("bspwm" "sxhkd" "nvim" "polybar" "ranger" "redshift" "dunst" "kitty" "neofetch" "rofi")
-ARCH_LOCAL_DOTFILES=("bin")
+#ARCH_DOTFILES=(".xinitrc" ".screenlayout" ".zshrc" ".zprofile" ".bash_profile" ".gitconfig")
+#ARCH_CONFIG_DOTFILES=("bspwm" "sxhkd" "nvim" "polybar" "ranger" "redshift" "dunst" "kitty" "neofetch" "rofi")
+ARCH_CONFIG_DOTFILES=("i3")
+#ARCH_LOCAL_DOTFILES=("bin")
 
 function check_internet_connection() {
 	ping -c 1 google.com
@@ -26,71 +27,69 @@ function check_internet_connection() {
 	fi
 }
 
+function determine_distro() {
+	# Determine Current Distro
 
-check_internet_connection
+	if [ -e /bin/apt ]
+	then
+		echo "Setting up config files for Debain-Based Distros"
+		sudo apt update
 
-# Determine Current Distro
-
-if [ -e /bin/apt ]
-then
-	echo "Setting up config files for Debain-Based Distros"
-	sudo apt update
-	
 #Install main packages on Debian-based Distros
 
-	for PKGS in ${GLOBAL_PACKAGES[@]}
-	do
-		sudo apt install $PKGS
-		echo "Installing ${PKGS}..."
-	done
-	
+for PKGS in ${GLOBAL_PACKAGES[@]}
+do
+	sudo apt install $PKGS
+	echo "Installing ${PKGS}..."
+done
+
 #Install Node specific packages on Debian-based Distros
 
-	for PKGS in ${REQUIRED_NODE_PACKAGES[@]}
-	do
-		sudo apt install $PKGS
-		echo "Installing ${PKGS}..."
-	done
+for PKGS in ${REQUIRED_NODE_PACKAGES[@]}
+do
+	sudo apt install $PKGS
+	echo "Installing ${PKGS}..."
+done
 
 #Install Miscallenous
-	
-	sudo npm install --global yarn
-	sudo pip3 install -U Commitizen
+
+sudo npm install --global yarn
+sudo pip3 install -U Commitizen
 
 
 ## Symlink Debain Specific config dotfiles
 
 # Symlink files within dotfiles dir to $USER dir
 
-	for FILES in ${DEBIAN_DOTFILES[@]}
-	do
-		ln -sf ~/Documents/dotfiles/$FILES ~/
-	done
+for FILES in ${DEBIAN_DOTFILES[@]}
+do
+	ln -sf ~/Documents/dotfiles/$FILES ~/
+done
 
 # Symlink config files within dotfiles dir to .config dir
 
-	for FILES in ${DEBIAN_CONFIG_DOTFILES[@]}
-	do
-		ln -sf ~/Documents/dotfiles/.config/$FILES ~/.config
-	done
+for FILES in ${DEBIAN_CONFIG_DOTFILES[@]}
+do
+	ln -sf ~/Documents/dotfiles/.config/$FILES ~/.config
+done
 
 
 # Success message
 
-	if [ "$?" -eq "0" ]
-	then
-		echo "You are all set!"
-		exit 0
-	else
-		echo "Something seems to be wrong..."
-		echo "Exiting Setup..."
-		exit 1
-	fi
-	
+if [ "$?" -eq "0" ]
+then
+	echo "You are all set!"
+	exit 0
+else
+	echo "Something seems to be wrong..."
+	echo "Exiting Setup..."
+	exit 1
+fi
+
 elif [ -e /bin/pacman.d ]
 then 
 	echo "Setting up config files for Arch-Based Distros"
-	
+
 # Install necessary dependecies for Arch Linux
 
 #(pacman -S base base-devel git wget htop bspwm 
@@ -108,13 +107,13 @@ else
 	mkdir ~/.config
 fi
 
-if [ -d ~/.local/ ]
-then
-	echo "Will symlink files to present .local directory"
-else
-	echo "making new .local directory..."
-	mkdir ~/.local/
-fi
+#if [ -d ~/.local/ ]
+#then
+#	echo "Will symlink files to present .local directory"
+#else
+#	echo "making new .local directory..."
+#	mkdir ~/.local/
+#fi
 
 # Symlink files in the dotfiles directory
 
@@ -139,5 +138,19 @@ done
 
 echo "Setup Succesful!!!"
 fi
+}
+
+function set_up_arch() {
+# Symlink files within directories in the dotfiles directory
+
+for files in "${ARCH_CONFIG_DOTFILES[@]}"
+do
+	ln -sf ~/dotfiles/.config/$files ~/.config/
+done
+}
+
+
+#check_internet_connection
+set_up_arch
 
 

88e7ff2d173561bdfdef2e22b4c180c6ac2da798
eniiku <@gmail.com>
2022-11-08 21:56:02 +0100

    chore(i3-Arch-config): move i3 config files to its seperate file
    Squashed commit of the following:
    
    commit ed5fc5de85e6a941321035576a988ff452e55d9e
    Author: eniiku <enikuauth@gmail.com>
    Date:   Tue Nov 8 21:53:05 2022 +0100
    
        chore(i3-Arch-config): rewrite config script to setup configurations for Arch i3
    
    commit 82c5228ab47df9c8f25651a5970ddb62e4212977
    Author: eniiku <enikuauth@gmail.com>
    Date:   Tue Nov 8 21:38:04 2022 +0100
    
        chore: import required config files
    
    close #3

---
+++ Arch/i3-Arch-config/config.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+I3_DOTFILES=(".zshrc" ".gitconfig")
+I3_CONFIG_DOTFILES=("i3" "i3status" "kitty" "polybar")
+
+function check_internet_connection() {
+	ping -c 1 google.com
+	  
+	if [ "$?" -eq "0" ]
+	then
+		echo "Confirmed Internet access!"
+		echo "Proceeding with configuration..."
+	else
+		echo "You don't seem to have a stable interet connection"
+		echo "Exiting configuration..."
+		exit 1
+	fi
+}
+
+function check_status() {
+	# Success message
+
+	if [ "$?" -eq "0" ]
+	then
+		echo "You are all set!"
+		exit 0
+	else
+		echo "Something seems to be wrong..."
+		echo "Exiting Setup..."
+		exit 1
+	fi
+}
+
+
+function set_up_i3() {
+	
+	echo "Configuring i3..."
+
+	# Check for existing .config directory
+	if [ -d ~/.config/ ]
+	then
+		echo "Will symlink files to present .config directory"
+	else
+		echo "making new .config directory..."
+		mkdir ~/.config
+	fi
+
+	#Symlink files to home directory
+	for files in "${I3_DOTFILES[@]}"
+	do
+		ln -sf ~/dotfiles/Arch/i3-Arch-config/$files ~/
+	done
+
+	# Symlink files within .config to home directory
+	for files in "${I3_CONFIG_DOTFILES[@]}"
+	do
+		ln -sf ~/dotfiles/Arch/i3-Arch-config/.config/$files ~/.config/
+	done
+}
+
+
+#check_internet_connection
+set_up_i3
+check_status

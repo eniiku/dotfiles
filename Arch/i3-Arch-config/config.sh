ed5fc5de85e6a941321035576a988ff452e55d9e
eniiku <@gmail.com>
2022-11-08 21:53:05 +0100

    chore(i3-Arch-config): rewrite config script to setup configurations for Arch i3

--- Arch/i3-Arch-config/config.sh
+++ Arch/i3-Arch-config/config.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# Debian-Based Distros
-
-#GLOBAL_PACKAGES=("nodejs" "npm" "neovim" "code" "kitty" "default-jdk" "python3-pip" "neofetch")
-#REQUIRED_NODE_PACKAGES=("yarn")
-
-DEBIAN_DOTFILES=(".bashrc")
-DEBIAN_CONFIG_DOTFILES=("nvim" "kitty" "neofetch")
-
-#ARCH_DOTFILES=(".xinitrc" ".screenlayout" ".zshrc" ".zprofile" ".bash_profile" ".gitconfig")
-#ARCH_CONFIG_DOTFILES=("bspwm" "sxhkd" "nvim" "polybar" "ranger" "redshift" "dunst" "kitty" "neofetch" "rofi")
-ARCH_CONFIG_DOTFILES=("i3" "i3status" "kitty" "polybar")
-#ARCH_LOCAL_DOTFILES=("bin")
+I3_DOTFILES=(".zshrc" ".gitconfig")
+I3_CONFIG_DOTFILES=("i3" "i3status" "kitty" "polybar")
 
 function check_internet_connection() {
 	ping -c 1 google.com
@@ -27,130 +17,48 @@ function check_internet_connection() {
 	fi
 }
 
-function determine_distro() {
-	# Determine Current Distro
+function check_status() {
+	# Success message
 
-	if [ -e /bin/apt ]
+	if [ "$?" -eq "0" ]
 	then
-		echo "Setting up config files for Debain-Based Distros"
-		sudo apt update
-
-#Install main packages on Debian-based Distros
-
-for PKGS in ${GLOBAL_PACKAGES[@]}
-do
-	sudo apt install $PKGS
-	echo "Installing ${PKGS}..."
-done
-
-#Install Node specific packages on Debian-based Distros
-
-for PKGS in ${REQUIRED_NODE_PACKAGES[@]}
-do
-	sudo apt install $PKGS
-	echo "Installing ${PKGS}..."
-done
-
-#Install Miscallenous
-
-sudo npm install --global yarn
-sudo pip3 install -U Commitizen
-
-
-## Symlink Debain Specific config dotfiles
-
-# Symlink files within dotfiles dir to $USER dir
-
-for FILES in ${DEBIAN_DOTFILES[@]}
-do
-	ln -sf ~/Documents/dotfiles/$FILES ~/
-done
-
-# Symlink config files within dotfiles dir to .config dir
-
-for FILES in ${DEBIAN_CONFIG_DOTFILES[@]}
-do
-	ln -sf ~/Documents/dotfiles/.config/$FILES ~/.config
-done
-
-
-# Success message
-
-if [ "$?" -eq "0" ]
-then
-	echo "You are all set!"
-	exit 0
-else
-	echo "Something seems to be wrong..."
-	echo "Exiting Setup..."
-	exit 1
-fi
-
-elif [ -e /bin/pacman.d ]
-then 
-	echo "Setting up config files for Arch-Based Distros"
-
-# Install necessary dependecies for Arch Linux
-
-#(pacman -S base base-devel git wget htop bspwm 
-#sxhkd neovim polybar ranger redshift dunst)
-
-# Check which shell is currently running
-# if [echo $SHELL = /usr/bin/zsh ]...
-
-
-if [ -d ~/.config/ ]
-then
-	echo "Will symlink files to present .config directory"
-else
-	echo "making new .config directory..."
-	mkdir ~/.config
-fi
-
-#if [ -d ~/.local/ ]
-#then
-#	echo "Will symlink files to present .local directory"
-#else
-#	echo "making new .local directory..."
-#	mkdir ~/.local/
-#fi
-
-# Symlink files in the dotfiles directory
-
-for files in ""${ARCH_DOTFILES[@]}
-do
-	ln -sf ~/dotfiles/$files ~/
-done
-
-# Symlink files within directories in the dotfiles directory
-
-for files in "${ARCH_CONFIG_DOTFILES[@]}"
-do
-	ln -sf ~/dotfiles/.config/$files ~/.config/
-done
-
-# Symlink files within directories in the .local directory
-
-for files in "${ARCH_LOCAL_DOTFILES[@]}"
-do 
-	ln -sf ~/dotfiles/.local/$files ~/.local/
-done
-
-echo "Setup Succesful!!!"
-fi
+		echo "You are all set!"
+		exit 0
+	else
+		echo "Something seems to be wrong..."
+		echo "Exiting Setup..."
+		exit 1
+	fi
 }
 
-function set_up_arch() {
-# Symlink files within directories in the dotfiles directory
 
-for files in "${ARCH_CONFIG_DOTFILES[@]}"
-do
-	ln -sf ~/dotfiles/.config/$files ~/.config/
-done
+function set_up_i3() {
+	
+	echo "Configuring i3..."
+
+	# Check for existing .config directory
+	if [ -d ~/.config/ ]
+	then
+		echo "Will symlink files to present .config directory"
+	else
+		echo "making new .config directory..."
+		mkdir ~/.config
+	fi
+
+	#Symlink files to home directory
+	for files in "${I3_DOTFILES[@]}"
+	do
+		ln -sf ~/dotfiles/Arch/i3-Arch-config/$files ~/
+	done
+
+	# Symlink files within .config to home directory
+	for files in "${I3_CONFIG_DOTFILES[@]}"
+	do
+		ln -sf ~/dotfiles/Arch/i3-Arch-config/.config/$files ~/.config/
+	done
 }
 
 
 #check_internet_connection
-set_up_arch
-
-
+set_up_i3
+check_status
